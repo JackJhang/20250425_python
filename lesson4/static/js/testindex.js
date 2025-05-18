afac30ca97ac76fe3079e18b65606847834593a7
Jack Jhang <uciskj8466@gmail.com>
2025-05-18 23:48:45 +0800

    modify

--- lesson4/static/js/testindex.js
+++ lesson4/static/js/testindex.js
@@ -1,6 +1,6 @@
-const menuButton = document.querySelector('.menu-button');  // 選取 HTML 中 class 為 "menu-button" 的元素，即 MENU 按鈕
-const navLinks = document.querySelector('.nav-links');      // 選取 HTML 中 class 為 "nav-links" 的元素，即導覽連結列表
+const menuButton = document.querySelector('.menu-button');
+const navLinks = document.querySelector('.nav-links');
 
-menuButton.addEventListener('click', () => {           // 為 MENU 按鈕新增一個點擊事件監聽器
-    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex'; // 切換導覽連結列表的顯示狀態：如果目前是 flex（顯示），就改為 none（隱藏），反之亦然
+menuButton.addEventListener('click', () => {
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
 });
